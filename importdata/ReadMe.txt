8ace7cae460479ab33c31cd65cdd1d90186507d7
rezash86 <shalchian.r@gmail.com>
2017-08-17 23:54:23 -0400

    adding Readme and update the dbScript

---
+++ importdata/ReadMe.txt
@@ -0,0 +1,5 @@
+1- use java 1.8 .
+2- with maven we can have the dependencies
+3- dbSql shows the script for db (creating db and creating table)
+4- Beans.Xml is used for config of db- just use your password after creating the db
+5- Run either Application.java which has main method or Run ImportDbServiceTest.java (should be Green :)) )
--- importdata/dbSql.sql
+++ importdata/dbSql.sql
@@ -1,3 +1,14 @@
+CREATE DATABASE "importDb"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+       
+       
+
+
 CREATE TABLE miso_market_price_five_minutes
 (
 	originaldatetime timestamp without time zone NOT NULL,
